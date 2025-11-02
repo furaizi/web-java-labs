6c5546a6dfb51a38d9fabafbbfd4a18aef7aa2ed
furaizi <furachannel16@gmail.com>
2025-11-02 14:28:34 +0200

    Lab1.1: initialize project template

---
+++ Lab1_1/src/main/kotlin/org/example/lab1_1/Lab11Application.kt
@@ -0,0 +1,11 @@
+package org.example.lab1_1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lab11Application
+
+fun main(args: Array<String>) {
+    runApplication<Lab11Application>(*args)
+}
