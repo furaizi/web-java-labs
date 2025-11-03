abe6d741a0702a280b30e695690980b55c534503
furaizi <furachannel16@gmail.com>
2025-11-03 21:48:50 +0200

    Lab1.2: rename main application class to Lab12Application

--- Lab1_2/src/main/kotlin/org/example/lab1_2/Lab12Application.kt
+++ Lab1_2/src/main/kotlin/org/example/lab1_2/Lab12Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Lab11Application
+class Lab12Application
 
 fun main(args: Array<String>) {
-    runApplication<Lab11Application>(*args)
+    runApplication<Lab12Application>(*args)
 }
