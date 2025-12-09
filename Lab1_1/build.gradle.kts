4ef004badab19844977caeef31ee593adc086856
furaizi <furachannel16@gmail.com>
2025-12-09 20:15:41 +0200

    Lab4: add JWT-based security using Spring Security and OAuth2 resource server

--- Lab1_1/build.gradle.kts
+++ Lab1_1/build.gradle.kts
@@ -26,6 +26,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
     implementation("org.liquibase:liquibase-core")
     runtimeOnly("org.postgresql:postgresql")
